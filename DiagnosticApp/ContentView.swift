25069d6cb8cc9223bc1c28763a743682f0ef6855
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-11-16 09:51:43 -0500

    Initial Commit

---
+++ DiagnosticApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiagnosticApp
+//
+//  Created by Dominic Lanzillotta on 11/16/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
